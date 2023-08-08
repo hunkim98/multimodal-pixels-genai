cdd1993be80a498c17a94f2bdafb778bed8e285e
Kim Dong Hun <hunkim98@gmail.com>
2023-08-09 01:35:10 +0900

    Remove s3 credentials

--- next.config.js
+++ next.config.js
@@ -58,9 +58,9 @@ const nextConfig = {
   ],
   env: {
     REPLICATE_TOKEN: process.env.REPLICATE_TOKEN,
-    S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
-    S3_REGION: process.env.S3_REGION,
-    S3_SECRET_KEY: process.env.S3_SECRET_KEY,
+    // S3_ACCESS_KEY: process.env.S3_ACCESS_KEY,
+    // S3_REGION: process.env.S3_REGION,
+    // S3_SECRET_KEY: process.env.S3_SECRET_KEY,
   },
 };
 
