2ab35da467dd7b3c06badef565815ab292133980
Kim Dong Hun <hunkim98@gmail.com>
2023-08-08 16:48:32 +0900

    Style for brush

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,9 +8,15 @@ import {
   Heading,
 } from "@adobe/react-spectrum";
 import ChveronRightMedium from "@spectrum-icons/ui/ChevronRightMedium";
+import BrushIcon from "@spectrum-icons/workflow/Brush";
+import EraserIcon from "@spectrum-icons/workflow/Erase";
+import DeleteIcon from "@spectrum-icons/workflow/Delete";
 import {
   BrushTool,
+  CanvasBrushChangeHandler,
+  CanvasDataChangeHandler,
   CanvasDataChangeParams,
+  CanvasStrokeEndHandler,
   Dotting,
   DottingData,
   DottingRef,
@@ -18,6 +24,7 @@ import {
   useBrush,
   useData,
   useDotting,
+  useHandlers,
 } from "dotting";
 import { parseColor } from "@react-stately/color";
 
@@ -27,22 +34,30 @@ import { generateOutputs } from "@/utils/kandinsky";
 import { ModelInputs } from "@/types/replicate";
 import axios from "axios";
 import Head from "next/head";
+import Brush from "@spectrum-icons/workflow/Brush";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [isPixelCanvasOpen, setIsPixelCanvasOpen] = useState(false);
   const dottingRef = useRef<DottingRef>(null);
+  const [brushTool, setBrushTool] = useState(BrushTool.DOT);
   let [currentValue, setCurrentValue] = useState(
     parseColor("hsl(50, 100%, 50%)")
   );
-  const [recentlyUsedColors, setRecentlyUsedColors] = useState<string[]>([]);
+  const [recentlyUsedColors, setRecentlyUsedColors] = useState<Set<string>>(
+    new Set()
+  );
   const [userDataArray, setUserDataArray] =
     useState<Array<Array<PixelModifyItem>>>();
   let [finalValue, setFinalValue] = useState(parseColor("hsl(50, 100%, 50%)"));
-  const { dataArray, data } = useData(dottingRef);
   const { clear, setData } = useDotting(dottingRef);
-  const { changeBrushColor, changeBrushTool, brushTool } = useBrush(dottingRef);
+  const {
+    addDataChangeListener,
+    addStrokeEndListener,
+    addBrushChangeListener,
+  } = useHandlers(dottingRef);
+  const { changeBrushColor, changeBrushTool } = useBrush(dottingRef);
   const [galleryImage, setGalleryImages] = useState<Array<string>>([]);
   const [modelInputs, setModelInputs] = useState<ModelInputs>({
     prompt: "A robot sitting on the ground",
@@ -53,16 +68,11 @@ export default function Home() {
       const images = res.data as Array<string>;
       console.log(res.data as Array<string>);
       setGalleryImages((prev) => [...prev, ...images]);
-
-      // setGalleryImages(res.data);
     });
-    // generateOutputs(modelInputs).then((output) => {
-    //   setOutput(output);
-    // });
   }, [modelInputs]);
 
   useEffect(() => {
-    const listener = ({ data }: CanvasDataChangeParams) => {
+    const dataChangeListener: CanvasDataChangeHandler = ({ data }) => {
       if (data.size === 0) {
         return;
       }
@@ -89,22 +99,56 @@ export default function Home() {
       }
       setUserDataArray(tempArray);
     };
-    const dottingRefCurrnet = dottingRef.current;
-    dottingRefCurrnet?.addDataChangeListener(listener);
-    // return () => {
-    //   if (dottingRefCurrnet) {
-    //     dottingRefCurrnet?.removeDataChangeListener(listener);
-    //   }
-    // };
-  }, [isPixelCanvasOpen, dottingRef]);
+    addDataChangeListener(dataChangeListener);
+  }, [isPixelCanvasOpen, addDataChangeListener]);
 
   useEffect(() => {
-    console.log("heyhyh");
-  }, [dataArray]);
+    const strokeEndListener: CanvasStrokeEndHandler = ({
+      strokedPixels,
+      strokeTool,
+    }) => {
+      if (strokeTool === BrushTool.DOT) {
+        const color = strokedPixels[strokedPixels.length - 1].color;
+        setRecentlyUsedColors((prev) => {
+          const newSet = new Set(prev);
+          newSet.add(color);
+          return newSet;
+        });
+      }
+    };
+    addStrokeEndListener(strokeEndListener);
+  }, [isPixelCanvasOpen, addStrokeEndListener]);
+
+  useEffect(() => {
+    if (recentlyUsedColors.size > 5) {
+      const newSet = new Set(recentlyUsedColors);
+      newSet.delete(Array.from(recentlyUsedColors)[0]);
+      setRecentlyUsedColors(newSet);
+    }
+  }, [recentlyUsedColors]);
+
+  useEffect(() => {
+    const brushToolListener: CanvasBrushChangeHandler = ({ brushTool }) => {
+      setBrushTool(brushTool);
+    };
+    addBrushChangeListener(brushToolListener);
+  }, [isPixelCanvasOpen, addBrushChangeListener, setBrushTool]);
+
+  useEffect(() => {
+    if (!isPixelCanvasOpen) {
+      setBrushTool((prev) => {
+        if (prev !== BrushTool.DOT) {
+          return BrushTool.DOT;
+        }
+        return prev;
+      });
+    }
+  }, [isPixelCanvasOpen, setBrushTool]);
 
   useEffect(() => {
     changeBrushColor(finalValue.toString("hex"));
   }, [finalValue, changeBrushColor]);
+
   return (
     <main className={`flex min-h-screen flex-col p-24 ${inter.className}`}>
       <div className="flex flex-row justify-center align-middle">
@@ -165,9 +209,10 @@ export default function Home() {
                     }}
                   />
                 </div>
-                <div className="flex flex-col align-middle px-5 pt-3">
-                  <Flex gap={"size-100"}>
+                <div className="flex flex-col align-middle px-3 pt-3">
+                  <Flex justifyContent={"space-between"}>
                     <Button
+                      width={"size-1200"}
                       variant="primary"
                       onPress={() => {
                         if (brushTool === BrushTool.ERASER) {
@@ -177,19 +222,53 @@ export default function Home() {
                         }
                       }}
                     >
-                      Brush
+                      {brushTool === BrushTool.ERASER ? (
+                        <EraserIcon />
+                      ) : (
+                        <BrushIcon />
+                      )}
+                      <Text UNSAFE_className="ml-1">
+                        {brushTool === BrushTool.ERASER ? "Eraser" : "Brush"}
+                      </Text>
                     </Button>
-                    <Button variant="primary" onPress={() => clear()}>
-                      Clear
+                    <Button
+                      width={"size-600"}
+                      variant="secondary"
+                      onPress={() => clear()}
+                    >
+                      <DeleteIcon
+                        size="XXS"
+                        UNSAFE_style={{
+                          opacity: 0.5,
+                        }}
+                      />
                     </Button>
                   </Flex>
                   <ColorWheel
+                    UNSAFE_className="my-5"
                     defaultValue="hsl(30, 100%, 50%)"
+                    value={currentValue}
                     onChange={setCurrentValue}
                     onChangeEnd={setFinalValue}
                   />
-                  <Flex>
-                    <Text>Brush</Text>
+                  <Flex direction="column">
+                    <Text UNSAFE_className="text-xs mb-1">
+                      Recently Used Colors
+                    </Text>
+                    <Flex gap="size-100">
+                      {Array.from(recentlyUsedColors).map((color) => (
+                        <div
+                          key={color}
+                          className={`w-6 h-6 rounded-full`}
+                          style={{
+                            backgroundColor: color,
+                          }}
+                          onClick={() => {
+                            setFinalValue(parseColor(color));
+                          }}
+                        />
+                      ))}
+                    </Flex>
                   </Flex>
                 </div>
               </div>
